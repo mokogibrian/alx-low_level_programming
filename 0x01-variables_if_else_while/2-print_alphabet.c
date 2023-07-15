839713b7b61e250f2a3a77a8bcdeeccf1c45acf6
mokogibrian <brianmokogiz@gmail.com>
2023-07-15 09:55:54 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,17 @@
 
 int main(void)
 {
-	char ch = 'a';
-	while (ch <= 'z');
-	{
-		putchar (ch); ++ch;
-	}
-		putchar ('\n');
-	return (0);
-}
+	char ch
 
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
