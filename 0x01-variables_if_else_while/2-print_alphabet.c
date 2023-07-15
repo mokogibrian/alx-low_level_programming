f941732ac8ed0718dd89953e3663c9c8ff9ece66
mokogibrian <brianmokogiz@gmail.com>
2023-07-15 09:01:53 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lower case
+ *
+ * Return - always zero (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+		while (ch <='z');
+		{
+			putchar (ch); ++ch;
+		}
+			putchar ('\n')
+	return (0);
+}
+
