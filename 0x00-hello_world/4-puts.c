684445d17799868e770583e5a42ba59a16bec28a
mokogibrian <brianmokogiz@gmail.com>
2023-07-16 13:00:50 -0700

    Hello World tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints
+ *
+ * This file prints programming
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char string1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string1);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints
+ *
+ * This file prints programming
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
