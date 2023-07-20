24583bedeef0266a9fb27f83f9538726e09a79bf
mokogibrian <brianmokogiz@gmail.com>
2023-07-20 06:28:26 -0700

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,17 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-
+	
 	i = 0;
-	while (i <10)
+	while (i < 10)
 	{
-		c = 'a';
-		while (c <= 'z')
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(c);
-			c++;
+			_putchar(ch);
+			ch++;
 		}
-		_putchar('\n');
-		i++;
+	_putchar('\n');
+	i++;
 	}
 }
