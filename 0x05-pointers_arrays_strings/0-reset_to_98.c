6ce2598ae302ceb0ce9dff3cf85713145a3f0f49
mokogibrian <brianmokogiz@gmail.com>
2023-07-24 14:50:56 -0700

    Corrected pointer code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,4 @@
-/** 
-  * reset_to_98 - takes a pointer to an int parameter 
+ * reset_to_98 - takes a pointer to an int parameter 
   * and updates the value it points to 98 
   * 
   * @n: int parameter 
